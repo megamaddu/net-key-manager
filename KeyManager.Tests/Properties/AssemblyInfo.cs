243dc6fc3fb83e35e41d0101f856c9ead914f07e
Michael Trotter <spicydonuts@gmail.com>
2013-12-03 13:52:57 -0700

    more renaming

--- KeyManager.Tests/Properties/AssemblyInfo.cs
+++ KeyManager.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("net-key-manager.Tests")]
+[assembly: AssemblyTitle("KeyManager.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("net-key-manager.Tests")]
+[assembly: AssemblyProduct("KeyManager.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
